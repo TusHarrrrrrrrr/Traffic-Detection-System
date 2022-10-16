58544e60f03a9e2feffc949c196a80561fd8eea4
TusHarrrrrrrrr <tushartib263139@gmail.com>
2022-10-16 17:19:50 +0530

    adding menu options

--- ui/window.py
+++ ui/window.py
@@ -7,8 +7,21 @@ class Window(Frame):
 
         self.master = master
         self.setup_window()
+        self.createMenuOptions()
 
     def setup_window(self):
         w, h = self.master.winfo_screenwidth(), self.master.winfo_screenheight()
         self.master.geometry("%dx%d+0+0" % (w, h))
         self.master.title(self.title)
+
+    def createMenuOptions(self):
+        menu = Menu(self.master)
+        self.master.config(menu=menu)
+
+        file = Menu(menu)
+        file.add_command(label="Open Video", command=self.open_file)
+        file.add_command(label="Exit App", command=exit)
+        menu.add_cascade(label="File", menu=file)
+
+    def open_file(self):
+        pass
